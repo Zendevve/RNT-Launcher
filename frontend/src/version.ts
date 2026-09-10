774ff89cb81e5b3f98443f136a76c4a053286c6f
Zendevve <nathanielguinto6@gmail.com>
2026-09-10 10:37:56 +0800

    chore(release): stamp displayed version v0.1.4-build.20260910.1

--- frontend/src/version.ts
+++ frontend/src/version.ts
@@ -1,3 +1,3 @@
-export const APP_VERSION = '0.1.3';
-export const BUILD_ID = 'build.20260905.1';
+export const APP_VERSION = '0.1.4';
+export const BUILD_ID = 'build.20260910.1';
 export const FULL_VERSION = `v${APP_VERSION}-${BUILD_ID}`;
